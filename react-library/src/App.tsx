fa98b3cde34d5602068e757f5c9b452f2877108e
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-11-29 17:22:40 +0100

    Cleaned up.

--- react-library/src/App.tsx
+++ react-library/src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import './App.css'
 import { Footer } from './layouts/footer/Footer'
-import { Carousel } from './layouts/homepage/Carousel'
-import { ExploreTopBooks } from './layouts/homepage/ExploreTopBooks'
-import { Heros } from './layouts/homepage/Heros'
-import { LibraryServices } from './layouts/homepage/LibraryServices'
+import { HomePage } from './layouts/homepage/HomePage'
 import { Navbar } from './layouts/navbar/Navbar'
 
-function App() {
+export const App = () => {
 	return (
 		<div>
 			<Navbar />
-			<ExploreTopBooks />
-      <Carousel />
-      <Heros />
-      <LibraryServices />
-      <Footer />
+			<HomePage />
+			<Footer />
 		</div>
 	)
 }
-
-export default App
