54a624aa69653272cf54b0973c10bde49ce850f5
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-12-01 21:28:16 +0100

    Added Search books component.

--- react-library/src/App.tsx
+++ react-library/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css'
 import { Footer } from './layouts/footer/Footer'
 import { HomePage } from './layouts/homepage/HomePage'
 import { Navbar } from './layouts/navbar/Navbar'
+import { SearchBooksPage } from './layouts/searchbookspage/SearchBooksPage'
 
 export const App = () => {
 	return (
 		<div>
 			<Navbar />
-			<HomePage />
+			{/* <HomePage /> */}
+			<SearchBooksPage />
 			<Footer />
 		</div>
 	)
